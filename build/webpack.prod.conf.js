a4e749b6eba0a4c20bfdb070ee3e5dfb57c1e836
admin <admin@admindeMacBook-Pro.local>
2017-08-02 22:52:48 +0800

    change build params

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -37,7 +37,8 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css')
+      filename: utils.assetsPath('css/[name].css') // no-hash-name
+      //filename: utils.assetsPath('css/[name].[contenthash].css')
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
